cb0138cb427f86bd607a65e5545f1ecd18410bfd
Faouzi Elbakri <56311060+Heelpme@users.noreply.github.com>
2020-04-20 00:47:42 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+		function toggle(){
+            var sec = document.getElementById('sec');
+            var team = document.getElementById('team');
+            var cours = document.getElementById('Cours');
+            var about = document.getElementById('about');
+            var contact = document.getElementById('contact');
+            var nav = document.getElementById('navigation');
+            var tog = document.getElementById('toggle');
+            sec.classList.toggle('active');
+            team.classList.toggle('active');
+            cours.classList.toggle('active');
+            about.classList.toggle('active');
+            contact.classList.toggle('active');
+            tog.classList.toggle('active');
+            nav.classList.toggle('active');
+			}
